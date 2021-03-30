7f54fae17563e3f0a58f99a4d79ca7c89cb020fb
Yash Suresh <psyys5@nottingham.ac.uk>
2021-03-30 18:03:08 +0100

    Initial

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';//enables JSX to work
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+function Welcome()
+{
+
+    return(
+        <div>
+            <h1>
+                To
+            </h1>
+        </div>
+    )//we can return JSX elements by wrapping them up in () and div
+}
+
+
+//the function render displays everything on the webpage.
+ReactDOM.render(
+    <Welcome/>,
+    document.getElementById('root')
+    //root is default 'hoster', anything we want to host needs to be on here
+    //ReactDOM.render(What do I want to render, where do i want to render it)
+);
+
+//JSX is a pseudo code which allows us to implement HTML elements in JS
